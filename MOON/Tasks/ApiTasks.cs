f2c8f14598093956c7be1ca07fff22139aaf7fd3
April P. <mewtek@icloud.com>
2025-01-30 17:16:22 -0800

    Boilerplate for WXNet API tasks

---
+++ MOON/Tasks/ApiTasks.cs
@@ -0,0 +1,23 @@
+namespace Moon.Tasks;
+
+public class ApiTasks
+{
+    /// <summary>
+    /// Grabs data refresh requests from the API every minute.
+    /// Also processes the process of distributing data refreshes and cleaning them up from the API.
+    /// </summary>
+    public static async Task CheckRequests()
+    {
+        // TODO:
+        return;
+    }
+
+    /// <summary>
+    /// Updates the current lists of applications and cleans up the ones that are not currently online/connected.
+    /// </summary>
+    public static async Task UpdateLocations()
+    {
+        // TODO:
+        return;
+    }
+}
