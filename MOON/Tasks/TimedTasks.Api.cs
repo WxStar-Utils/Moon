f8952f2264457379c673b30e322ef1a4b766c2ab
April P. <mewtek@icloud.com>
2025-03-12 20:21:57 -0700

    Send out data for newly added location keys.

--- MOON/Tasks/TimedTasks.Api.cs
+++ MOON/Tasks/TimedTasks.Api.cs
@@ -1,5 +1,6 @@
 using System.Reflection.Metadata.Ecma335;
 using Moon.API;
+using Moon.RecordGeneration;
 using Moon.Schema.System;
 
 namespace Moon.Tasks;
@@ -31,8 +32,35 @@ public partial class TimedTasks
             Log.Info("Refreshing Mist locations cache.");
 
             await Mist.GetActiveLocations();
-
+            await SendNewUnitData();
             await Task.Delay(300 * 1000);    // Checks every 5 minutes.
         }
+    }
+
+    /// <summary>
+    /// Compares current active locations to the existing locations cache, and sends out data for newly added location
+    /// keys.
+    /// </summary>
+    private static async Task SendNewUnitData()
+    {
+        List<string> newLocations = new List<string>();
+        
+        foreach (string location in Mist.Locations)
+        {
+            // Check the current locations cache to see if these are actually new locations
+            if (Globals.LocationCache.TryGetValue(location, out var locationData))
+            {
+                continue;
+            }
+            
+            newLocations.Add(location);
+        }
+        
+        Log.Info($"The following locations have been added to the database:");
+        Log.Info(string.Join(",", newLocations));
+
+        Publisher.PublishI2MRecords(newLocations.ToArray(), "i2m/global");
+        
+        Log.Info("Data has been sent out for all newly added locations.");
     }
 }
